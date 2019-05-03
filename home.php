2946c125202b48f2a06ac67996a60c4eef182f02
Thiago Goulart <thiaggoulart@gmail.com>
2019-05-03 10:57:23 -0300

    criação da home

---
+++ home.php
@@ -0,0 +1,119 @@
+<!DOCTYPE html>
+<html lang="pt-BR">
+<head>
+	<meta charset="utf-8">
+	<title>Bookstore</title>
+	<link rel="stylesheet" type="text/css" href="css/index.css">
+	<link rel="stylesheet" type="text/css" href="libs/bootstrap/css/bootstrap-reboot.min.css">
+	<link rel="stylesheet" href="libs/bootstrap/css/bootstrap.min.css">
+</head>
+<body>
+	<header id="cabecalho">
+      
+<!--Navbar-->
+<nav class="navbar navbar-light light-blue lighten-4">
+
+  <!-- Navbar brand -->
+  <a class="navbar-brand" href="#">Navbar</a>
+
+  <!-- Collapse button -->
+  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent1"
+    aria-controls="navbarSupportedContent1" aria-expanded="false" aria-label="Toggle navigation">
+    <span class="navbar-toggler-icon"></span></button>
+
+  <!-- Collapsible content -->
+  <div class="collapse navbar-collapse" id="navbarSupportedContent1">
+
+    <!-- Links -->
+    <ul class="navbar-nav mr-auto">
+      <li class="nav-item active">
+        <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="#">Features</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="#">Pricing</a>
+      </li>
+    </ul>
+    <!-- Links -->
+
+  </div>
+  <!-- Collapsible content -->
+
+</nav>
+<!--/.Navbar-->
+  <img id="carrinho" src="https://cdn.icon-icons.com/icons2/494/PNG/512/cart_icon-icons.com_48341.png" alt="carrinho">
+</nav>
+
+      
+    </header>
+
+  <main id="main">
+    <div id="pesquisa">
+      <input  class="form-control" type="text" placeholder="Search" aria-label="Search">
+    </div>
+      <div class="card-group">
+        <div class="card">
+            <img src="https://images-submarino.b2w.io/produtos/01/00/item/115042/6/115042690_1GG.jpg" alt="box harry potter">
+            <div class="card-body">
+              <h5 class="card-title">Box Harry Potter</h5>
+              <p class="card-text">Box com 7 livros da saga Harry Potter.</p>
+              <strong><p class="card-text">R$ 99,99</p></strong>
+            </div>
+          </div>
+          <div class="card">
+            <img src="https://http2.mlstatic.com/box-3-livros-trilogia-as-pecas-infernais-cassandra-clare--D_NQ_NP_776092-MLB26272952835_112017-F.webp" alt="box as peças infernais">
+            <div class="card-body">
+              <h5 class="card-title">Box As Peças Infernais</h5>
+              <p class="card-text">Box com 3 livros da saga As Peças Infernais</p>
+              <strong><p class="card-text">R$ 89,99</p></strong>
+            </div>
+          </div>
+          <div class="card">
+            <img src="https://images-submarino.b2w.io/produtos/01/00/item/129536/2/129536201_3SZ.jpg" alt="box os instrumentos mortais">
+            <div class="card-body">
+              <h5 class="card-title">Box Os Instrumentos Mortais</h5>
+              <p class="card-text">Box com 6 livros da saga Os Instrumentos Mortais.</p>
+              <strong><p class="card-text">R$ 149,99</p></strong>
+            </div>
+          </div>
+      </div>
+      <div class="card-group">
+        <div class="card">
+            <img src="https://78.media.tumblr.com/65dbf48ee2eb5f28248e9b0b60dc8733/tumblr_nbeh5rooez1rfx93bo1_500.jpg" alt="box herois do olimpo">
+            <div class="card-body">
+              <h5 class="card-title">Box Herois do Olimpo</h5>
+              <p class="card-text">Box com 5 livros da saga Herois do Olimpo.</p>
+              <strong><p class="card-text">R$ 89,99</p></strong>
+            </div>
+          </div>
+          <div class="card">
+            <img src="https://www.traca.com.br/capas/943/943673.jpg" alt="box jogos vorazes">
+            <div class="card-body">
+              <h5 class="card-title">Box Jogos Vorazes</h5>
+              <p class="card-text">Box com 3 livros da saga Jogos Vorazes.</p>
+              <strong><p class="card-text">R$ 84,99</p></strong>
+            </div>
+          </div>
+          <div class="card">
+            <img src="https://images-na.ssl-images-amazon.com/images/I/51YGwxKn3CL.jpg" alt="box narnia">
+            <div class="card-body">
+              <h5 class="card-title">Box Nárnia</h5>
+              <p class="card-text">Box com 7 livros da saga Nárnia.</p>
+              <strong><p class="card-text">R$ 74,99</p></strong>
+            </div>
+          </div>
+      </div>
+    </main>
+
+  <footer>
+    <div id="copyright">
+          Copyright &copy;2019 by Lara e Thiago
+    </div>
+</footer>
+  <script src="libs/jquery.js"></script>
+	<script src="libs/popper.js"></script>
+	<script src="libs/bootstrap/js/bootstrap.min.js"></script>
+</body>
+</html>
