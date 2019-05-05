ef95e943b5a1c10fc7fe4a82df62838782b40154
unknown <thiaggoulart@gmail.com>
2019-05-04 21:54:52 -0300

    menu criado

---
+++ login.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="pt-BR">
+<head>
+	<meta charset="utf-8">
+	<title>Login</title>
+	<link rel="stylesheet" type="text/css" href="css/index.css">
+	<link rel="stylesheet" type="text/css" href="libs/bootstrap/css/bootstrap-reboot.min.css">
+	<link rel="stylesheet" href="libs/bootstrap/css/bootstrap.min.css">
+</head>
+<body>
+    <h1 id="login">Login</h1>
+    <form>
+        <div class="form-group">
+            <label for="exampleInputEmail1">E-mail:</label>
+            <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Digite seu e-mail...">
+        </div>
+        <div class="form-group">
+            <label for="exampleInputPassword1">Senha</label>
+            <input type="password" class="form-control" id="exampleInputPassword1" placeholder="Digite sua senha...">
+        </div>
+        <button type="submit" class="btn btn-dark" id="logar">Login</button>
+    </form>
+    <a href=""><h5>Cadastre-se</h5></a>
+</body>
+</html>
